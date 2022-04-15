2e3d914873138141c39849decddbb981d3ffc000
johnnybigoo <jotalico@icloud.com>
2022-04-14 23:32:40 -0400

    Memory class | pt01

--- calculator/lib/models/memory.dart
+++ calculator/lib/models/memory.dart
@@ -1,14 +1,40 @@
-import 'package:flutter/material.dart';
-
 class Memory {
+  static const operations = const ['AC', '%', '/', 'X', '-', '+', '='];
+
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  late String operation;
   String _value = '0';
+  bool _wipeValue = false;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
     } else {
-      _value += command;
+      _addDigit(command);
     }
+  }
+
+  _setOperation(String newOperation) {
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) {
+      return;
+    }
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   _allClear() {
--- calculator/lib/screens/calculator.dart
+++ calculator/lib/screens/calculator.dart
@@ -2,6 +2,7 @@
 
 import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 
@@ -21,12 +22,17 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Column(
-      children: <Widget>[
-        Display(memory.value),
-        Keyboard(_onPressed),
-      ],
-    ));
+          children: <Widget>[
+            Display(memory.value),
+            Keyboard(_onPressed),
+          ],
+        ));
   }
 }
