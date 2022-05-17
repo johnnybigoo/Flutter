27099c6c8051213386f4a8837c9da6e0164c2a09
johnnybigoo <jotalico@icloud.com>
2022-05-16 23:49:27 -0400

    Flutter Apprentice book | Chapter-02 - Creating an actual target page

--- flutterApprentice/recipes/lib/main.dart
+++ flutterApprentice/recipes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -72,8 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   MaterialPageRoute(
                     builder: (context) {
                       // 10
-                      // TODO: Replace return with return RecipeDetail()
-                      return Text('Detail page');
+                      return RecipeDetail(recipe: Recipe.samples[index]);
                     },
                   ),
                 );
