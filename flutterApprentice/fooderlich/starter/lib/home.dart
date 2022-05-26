45b40b20e45aaede129d2249a51bdf8cfbea2bc2
johnnybigoo <jotalico@icloud.com>
2022-05-25 23:48:47 -0400

    Flutter Apprentice book | Chapter-03 - Composing Card1: the main recipe card

--- flutterApprentice/fooderlich/starter/lib/home.dart
+++ flutterApprentice/fooderlich/starter/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'card1.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -14,8 +15,7 @@ class _HomeState extends State<Home> {
 
   // 8
   static List<Widget> pages = <Widget> [
-    // TODO: Replace with Card1
-    Container(color: Colors.red,),
+    const Card1(),
     // TODO: Replace with Card2
     Container(color: Colors.green,),
     // TODO: Replace with Card3
@@ -53,7 +53,7 @@ class _HomeState extends State<Home> {
         currentIndex: _selectedIndex,
         // 11
         onTap: _onItemTapped,
-        
+
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
