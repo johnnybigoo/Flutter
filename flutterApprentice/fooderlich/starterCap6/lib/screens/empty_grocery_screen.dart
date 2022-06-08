9e8469435645b14b0ede677a9870a600d8b5e44f
johnnybigoo <jotalico@icloud.com>
2022-06-07 20:55:58 -0400

    Flutter Apprentice book | Chapter-06 - Adding the visual pieces

--- flutterApprentice/fooderlich/starterCap6/lib/screens/empty_grocery_screen.dart
+++ flutterApprentice/fooderlich/starterCap6/lib/screens/empty_grocery_screen.dart
@@ -14,10 +14,35 @@ class EmptyGroceryScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // TODO 4: Add empty image
-            // TODO 5: Add empty screen title
-            // TODO 6: Add empty screen subtitle
-            // TODO 7: Add browse recipes button
+            // 1
+            Flexible(
+                // 2
+                child: AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+                ),
+            ),
+            const Text(
+              'No Groceries',
+                  style: TextStyle(fontSize: 21.0),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Shopping for ingredients?\n'
+                  'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+              textColor: Colors.white,
+              child: const Text('Brownse Recipes'),
+              shape: RoundedRectangleBorder(
+                borderRadius:  BorderRadius.circular(30.0),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                // TODO 8: Go to recipes Tab
+              },
+            ),
           ],
         ),
       ),
