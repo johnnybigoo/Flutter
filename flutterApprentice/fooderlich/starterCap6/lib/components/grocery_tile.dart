e60b459dd516577ef8c6ad6c72291030e267b4ef
johnnybigoo <jotalico@icloud.com>
2022-11-11 20:48:44 -0500

    Flutter Apprentice book | Chapter-06 - Showing the checkbox

--- flutterApprentice/fooderlich/starterCap6/lib/components/grocery_tile.dart
+++ flutterApprentice/fooderlich/starterCap6/lib/components/grocery_tile.dart
@@ -29,40 +29,42 @@ class GroceryTile extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // 2
-          Container(width: 5.0, color: item.color),
-          // 3
-          const SizedBox(width: 16.0),
-          // 4
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              // 5
+              Container(
+                width: 5.0,
+                color: item.color,
+              ),
+              const SizedBox(width: 16.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
               Text(
-                item.name,
+                item.quantity.toString(),
                 style: GoogleFonts.lato(
                   decoration: textDecoration,
                   fontSize: 21.0,
-                  fontWeight: FontWeight.bold),
+                ),
               ),
-              const SizedBox(height: 4.0),
-              buildDate(),
-              const SizedBox(height: 4.0),
-              buildImportance(),
-            ],
-          ),
-          // 6
-          Row(
-            children: [
-              // 7
-              Text(item.quantity.toString(),
-                style:
-                  GoogleFonts.lato(
-                    decoration: textDecoration,
-                    fontSize: 21.0),
-              ),
-              // 8
               buildCheckbox(),
             ],
           ),
