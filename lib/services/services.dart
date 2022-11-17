640db9b954bcdd3631892f7a6b00f9d91ec6b8c4
johnnybigoo <jotalico@icloud.com>
2022-11-17 00:15:37 -0500

    Flutter | MVVM example | part 01

---
+++ lib/services/services.dart
@@ -0,0 +1,21 @@
+
+import 'dart:convert';
+
+import 'package:flutter_mvvm_demo/model/picsum_model.dart';
+import 'package:http/http.dart' as http;
+
+class Services{
+  Future<List<PicSumModel>> fetchPicturesAPI() async {
+    String url = 'https://picsum.photos/v2/list';
+    final response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200)
+      {
+        final json = jsonEncode(response.body) as List<dynamic>;
+        final listResult = json.map((e) => PicSumModel.fromJson(e)).toList();
+        return listResult;
+      }
+    else {
+      throw Exception('Error fetching pictures');
+    }
+  }
+}
