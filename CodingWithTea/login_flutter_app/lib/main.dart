f4d06d48cca49356a721455a2fc98f1557f6892f
johnnybigoo <jotalico@icloud.com>
2022-12-06 23:55:01 -0500

    CodingWithTea | Flutter Login App | Custom Theme p1

--- CodingWithTea/login_flutter_app/lib/main.dart
+++ CodingWithTea/login_flutter_app/lib/main.dart
@@ -1,66 +1,93 @@
+// ignore_for_file: override_on_non_overriding_member, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primarySwatch: MaterialColor(0xFFFFE200, <int, Color>{
+          50: Color(0x1AFFE200),
+          100: Color(0x33FFE200),
+          200: Color(0x4DFFE200),
+          300: Color(0x66FFE200),
+          400: Color(0x80FFE200),
+          500: Color(0xFFFFE200),
+          600: Color(0x99FFE200),
+          700: Color(0xB3FFE200),
+          800: Color(0xCCFFE200),
+          900: Color(0xE6FFE200),
+        }),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      home: AppHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class AppHome extends StatelessWidget {
+  const AppHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text(".appable/"),
+        leading: const Icon(Icons.ondemand_video),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // ignore: prefer_const_constructors
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_shopping_cart_outlined),
+        onPressed: () {},
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            Text(
+              "Heading",
+              style: Theme.of(context).textTheme.headline2,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "Sub-Heading",
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+            Text(
+              "Paragraph",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Elevated Button"),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text("Outlined Button"),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Image(
+                image: AssetImage("assets/images/books.png"),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
