9a6fa5c5695143e8f8999a8902e23f562b3a00ed
Jones Tavares <jotalico@icloud.com>
2022-12-09 21:39:40 -0500

    Flutter Apprentice book | Chapter-08 | Converting an AppLink to a URL string

--- flutterApprentice/fooderlich/starterCap8/lib/navigation/app_link.dart
+++ flutterApprentice/fooderlich/starterCap8/lib/navigation/app_link.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/foundation.dart';
+
 class AppLink {
   // 1
   static const String kHomePath = '/home';
   static const String kOnboardingPath = '/onboarding';
   static const String kLoginPath = '/login';
+  static const String kProfilePath = '/profile';
   static const String kItemPath = '/item';
   // 2
   static const String kTabParam = '/tab';
@@ -40,6 +43,41 @@ class AppLink {
     // 6
     return link;
   }
-// TODO: Add toLocation
 
+  String toLocation() {
+    // 1
+    String addKeyValPair({
+      required String key,
+      String? value,
+    }) =>
+        value == null ? '' : '${key}=$value&';
+    // 2
+    switch (location) {
+      // 3
+      case kLoginPath:
+        return kLoginPath;
+      // 4
+      case kOnboardingPath:
+        return kOnboardingPath;
+      // 5
+      case kProfilePath:
+        return kProfilePath;
+      // 6
+      case kItemPath:
+        var loc = '$kItemPath?';
+        loc += addKeyValPair(
+          key: kTabParam,
+          value: currentTab.toString(),
+        );
+        return Uri.encodeFull(loc);
+      // 7
+      default:
+        var loc = '$kHomePath?';
+        loc += addKeyValPair(
+          key: kTabParam,
+          value: currentTab.toString(),
+        );
+        return Uri.encodeFull(loc);
+    }
+  }
 }
