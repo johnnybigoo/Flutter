2f4162bdeb6c033a6043b5b319b93f10edf19164
Jones Tavares <jotalico@icloud.com>
2022-12-14 23:47:26 -0500

    FutterBricks | Create a personal widget

---
+++ FlutterBricks/flutter_lab/lib/bricks/Buttons/button1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Button1 extends StatelessWidget {
+  const Button1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
