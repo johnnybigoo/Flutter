2c31cc9aaf16ff140f905a6f136e96bc62ae132c
johnnybigoo <jotalico@icloud.com>
2022-12-25 15:26:51 -0500

    FlutterMapp | Learn Flutter in 1 Hour | part_3

--- FlutterMapp/flutter_1_hour/lib/learn_flutter_page.dart
+++ FlutterMapp/flutter_1_hour/lib/learn_flutter_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LeanFlutterPage extends StatelessWidget {
+class LeanFlutterPage extends StatefulWidget {
   const LeanFlutterPage({super.key});
 
+  @override
+  State<LeanFlutterPage> createState() => _LeanFlutterPageState();
+}
+
+class _LeanFlutterPageState extends State<LeanFlutterPage> {
+  bool isSwitch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,86 @@ class LeanFlutterPage extends StatelessWidget {
       body: Column(
         children: [
           Image.asset('assets/images/angry_cat.jpeg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(
+              10,
+            ),
+            padding: const EdgeInsets.all(
+              10.0,
+            ),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint('Elevated Button');
+            },
+            child: const Text('Elevated Button'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('Outlined Button');
+            },
+            child: const Text('Outlined Button'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('Text Button');
+            },
+            child: const Text('Text Button'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint(
+                'This is the row',
+              );
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.red,
+                ),
+                Text(
+                  'Row Widget',
+                ),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          Switch(
+            value: isSwitch,
+            onChanged: (bool newBool) {
+              setState(() {
+                isSwitch = newBool;
+              });
+            },
+          ),
         ],
       ),
     );
