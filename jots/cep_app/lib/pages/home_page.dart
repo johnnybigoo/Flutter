5dadd5ba481708ff97a47c539b0e740bfb72a54f
Jones Tavares <jotalico@icloud.com>
2022-12-27 23:37:07 -0500

    Rodrigo Rahman | Seu Primeiro App em Flutter | Part 4

--- jots/cep_app/lib/pages/home_page.dart
+++ jots/cep_app/lib/pages/home_page.dart
@@ -13,6 +13,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final CepRepository cepRepository = CepRepositoryImpl();
   EnderecoModel? enderecoModel;
+  bool loading = false;
+
+  final formKey = GlobalKey<FormState>();
+  final cepEC = TextEditingController();
+
+  @override
+  void dispose() {
+    cepEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +32,56 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         child: Form(
+          key: formKey,
           child: Column(
             children: [
-              TextFormField(),
+              TextFormField(
+                controller: cepEC,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'CEP Obrigatorio';
+                  }
+                  return null;
+                },
+              ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  final valid = formKey.currentState?.validate() ?? false;
+                  if (valid) {
+                    try {
+                      setState(() {
+                        loading = true;
+                      });
+                      final endereco = await cepRepository.getCep(cepEC.text);
+                      setState(() {
+                        loading = false;
+                        enderecoModel = endereco;
+                      });
+                    } catch (e) {
+                      setState(() {
+                        loading = false;
+                        enderecoModel = null;
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        // ignore: prefer_const_constructors
+                        SnackBar(
+                          content: const Text('Erro ao buscar endereco.'),
+                        ),
+                      );
+                    }
+                  }
+                },
                 child: const Text('Buscar'),
               ),
-              Text(
-                'Logradouro Complemento Cep',
+              Visibility(
+                visible: loading == true,
+                child: const CircularProgressIndicator(),
+              ),
+              Visibility(
+                visible: enderecoModel != null,
+                child: Text(
+                  '${enderecoModel?.logradouro} \n ${enderecoModel?.complemento} \n ${enderecoModel?.cep}',
+                ),
               ),
             ],
           ),
