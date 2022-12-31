1173b10bb8ac6fb0272e36bed660ff0a37022293
johnnybigoo <jotalico@icloud.com>
2022-12-30 23:41:53 -0500

    Dart Apprentice | Chapter 01 | Hello, Dart

--- dartApprentice/hello_dart_project/bin/hello_dart_project.dart
+++ dartApprentice/hello_dart_project/bin/hello_dart_project.dart
@@ -1,5 +1,10 @@
-import 'package:hello_dart_project/hello_dart_project.dart' as hello_dart_project;
+import 'package:hello_dart_project/hello_dart_project.dart'
+    as hello_dart_project;
 
-void main(List<String> arguments) {
-  print('Hello world: ${hello_dart_project.calculate()}!');
+// void main(List<String> arguments) {
+//   print('Hello world: ${hello_dart_project.calculate()}!');
+// }
+
+void main() {
+  print('Hello, Dart Project!');
 }
