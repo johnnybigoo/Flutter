209a036e37ecb1b113b5cde882085cd3d8137c4f
Jones Tavares <jotalico@icloud.com>
2023-01-12 23:27:28 -0500

    Dart Apprentice | Chapter 02 | Types & Operations | 02

--- dartApprentice/chapter_03/types_operations/lib/types_operations.dart
+++ dartApprentice/chapter_03/types_operations/lib/types_operations.dart
@@ -3,5 +3,13 @@ int calculate() {
 }
 
 void main() {
-  print("test");
+  const myInteger = 10;
+  num myNumber = 3.14;
+  final someNumber = 3.1;
+  final someInt = someNumber.toInt();
+  // print("test");
+  // print(myNumber is double);
+  // print(myNumber is int);
+  print(myNumber.runtimeType);
+  print(someInt.isEven);
 }
