5846755defcbe51efea7fec4ac57070e47f38454
Jones Tavares <jotalico@icloud.com>
2023-02-02 00:01:56 -0500

    Dart Week 9 | Project Architecture Folders

--- Dart_Week/dw9_delivery_app/lib/app/dw9_delivery_app.dart
+++ Dart_Week/dw9_delivery_app/lib/app/dw9_delivery_app.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class Dw9DeliveryApp extends StatelessWidget {
@@ -6,9 +7,10 @@ class Dw9DeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Delivery App',
       routes: {
-        '/': (context) => Container(),
+        '/': (context) => const SplashPage(),
       },
     );
   }
