51ebf0fa8a0b07e363640d6b8b314e136673bb7e
Jones Tavares <jotalico@icloud.com>
2023-02-04 19:21:47 -0500

    Dart Week 9 | theme_config

--- Dart_Week/dw9_delivery_app/lib/app/dw9_delivery_app.dart
+++ Dart_Week/dw9_delivery_app/lib/app/dw9_delivery_app.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ class Dw9DeliveryApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Delivery App',
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
       },
--- Dart_Week/dw9_delivery_app/lib/app/pages/splash/splash_page.dart
+++ Dart_Week/dw9_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,34 +10,18 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        primaryColor: ColorsApp.i.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorsApp.i.primary,
-          secondary: ColorsApp.i.secondary,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: AppStyles.i.primaryButton,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Splash'),
-        ),
-        body: Column(
-          children: [
-            Container(),
-            ElevatedButton(onPressed: () {}, child: Text('Test'))
-          ],
-        ),
+      body: Column(
+        children: [
+          Container(),
+          DeliveryButton(),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'text'),
+          ),
+        ],
       ),
     );
   }
