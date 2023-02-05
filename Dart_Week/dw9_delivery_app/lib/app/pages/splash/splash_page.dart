f81f9e60aa8ee8cc2d135e27796d45834039b213
Jones Tavares <jotalico@icloud.com>
2023-02-05 12:05:07 -0500

    Dart Week 9 | Helpers

--- Dart_Week/dw9_delivery_app/lib/app/pages/splash/splash_page.dart
+++ Dart_Week/dw9_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -22,6 +23,21 @@ class SplashPage extends StatelessWidget {
             height: 200,
             label: Env.i['backend_base_url'] ?? '',
             onPressed: () {},
+          ),
+          Text(context.screenWidth.toString()),
+          Row(
+            children: [
+              Container(
+                color: Colors.red,
+                width: context.percentWidth(.6),
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                width: context.percentWidth(.4),
+                height: 200,
+              ),
+            ],
           ),
           TextFormField(
             decoration: InputDecoration(labelText: 'text'),
