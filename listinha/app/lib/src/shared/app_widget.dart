67109ad16524013c32a98e804bffbe98b1e0cbbe
Jones Tavares <jotalico@icloud.com>
2023-02-09 00:18:54 -0500

    Flutterando | Semana Fullstack | Dia-02

---
+++ listinha/app/lib/src/shared/app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/src/shared/themes/themes.dart';
+
+import '../../main.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const MyHomePage(title: 'LISTINHA'),
+    );
+  }
+}
