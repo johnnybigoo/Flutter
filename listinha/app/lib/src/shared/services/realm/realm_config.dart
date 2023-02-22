80fbe1781027d280078b1f622f87607d3b927f3d
Jones Tavares <jotalico@icloud.com>
2023-02-21 19:52:01 -0500

    Flutterando | Semana Fullstack | Dia-08 - Relacionamento de tableas no Realm

--- listinha/app/lib/src/shared/services/realm/realm_config.dart
+++ listinha/app/lib/src/shared/services/realm/realm_config.dart
@@ -1,3 +1,4 @@
+import 'package:listinha/src/shared/services/realm/models/task_model.dart';
 import 'package:realm/realm.dart';
 
 import 'models/configuration_model.dart';
@@ -5,9 +6,11 @@ import 'models/configuration_model.dart';
 LocalConfiguration config = Configuration.local(
   [
     ConfigurationModel.schema,
+    Task.schema,
+    TaskBoard.schema,
   ],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
   },
-  path: './realm/realm.db',
+  // path: './realm/realm.db',
 );
