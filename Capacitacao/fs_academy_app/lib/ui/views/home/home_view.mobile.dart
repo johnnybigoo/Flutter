4e5f87421f04da6ced5409058d1f1b7a2f4f8ea0
Jones Tavares <jotalico@icloud.com>
2023-03-13 18:26:35 -0400

    Filledstacks | Responsible UI App 2023 | Responsive UI

--- Capacitacao/fs_academy_app/lib/ui/views/home/home_view.mobile.dart
+++ Capacitacao/fs_academy_app/lib/ui/views/home/home_view.mobile.dart
@@ -1,6 +1,12 @@
 import 'package:fs_academy_app/ui/common/app_colors.dart';
 import 'package:fs_academy_app/ui/common/ui_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/common/academy_icon.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/common/input_field.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/home_image.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/home_notify_button.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/home_subtitle.dart';
+import 'package:fs_academy_app/ui/views/home/widgets/home_title.dart';
 import 'package:stacked/stacked.dart';
 
 import 'home_viewmodel.dart';
@@ -11,64 +17,25 @@ class HomeViewMobile extends ViewModelWidget<HomeViewModel> {
   @override
   Widget build(BuildContext context, HomeViewModel viewModel) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                verticalSpaceLarge,
-                Column(
-                  children: [
-                    const Text(
-                      'Hello, MOBILE UI!',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    verticalSpaceMedium,
-                    MaterialButton(
-                      color: Colors.black,
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: viewModel.showDialog,
-                    ),
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: viewModel.showBottomSheet,
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
+      backgroundColor: kcBackgroundColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 50,
         ),
+        children: const [
+          AcademyIcon(),
+          verticalSpaceLarge,
+          HomeTitle(),
+          verticalSpaceTiny,
+          HomeSubtitle(),
+          verticalSpaceLarge,
+          InputField(),
+          verticalSpaceMedium,
+          HomeNotifyButton(),
+          verticalSpaceMedium,
+          HomeImage(),
+        ],
       ),
     );
   }
