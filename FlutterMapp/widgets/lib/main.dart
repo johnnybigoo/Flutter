4e5716939e35f61434595e17a0f75e38267477c7
Jones Tavares <jotalico@icloud.com>
2023-03-23 11:51:00 -0400

    FlutterMapp | Every Widget | AnimatedRotation

--- FlutterMapp/widgets/lib/main.dart
+++ FlutterMapp/widgets/lib/main.dart
@@ -16,7 +16,8 @@ import 'package:flutter/material.dart';
 // import 'package:widgets/widgets/animated_opacity.dart';
 // import 'package:widgets/widgets/animated_padding.dart';
 // import 'package:widgets/widgets/animated_physical_modal.dart';
-import 'package:widgets/widgets/animated_positioned.dart';
+// import 'package:widgets/widgets/animated_positioned.dart';
+import 'package:widgets/widgets/animated_rotation.dart';
 
 void main() {
   runApp(const MyApp());
