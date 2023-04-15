5d25f9f6256aadde8324172d3c7937d255daa184
Jones Tavares <jotalico@icloud.com>
2023-04-15 13:42:43 -0400

    FlutterMapp | Every Widget | cupertino_action_sheetAction.dart

---
+++ FlutterMapp/widgets/lib/widgets/C/cupertino_action_sheetAction.dart
@@ -0,0 +1,46 @@
+//!CuertinoActionSheetAction
+
+import 'package:flutter/cupertino.dart';
+
+class Scene extends StatefulWidget {
+  const Scene({super.key});
+
+  @override
+  State<Scene> createState() => _SceneState();
+}
+
+class _SceneState extends State<Scene> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: CupertinoButton(
+          child: const Text('CupertinoActionSheet'),
+          onPressed: () {
+            showCupertinoModalPopup(
+              context: context,
+              builder: (BuildContext context) => CupertinoActionSheet(
+                title: const Text('Flutter Mapp'),
+                message: const Text('Your Message'),
+                actions: <CupertinoActionSheetAction>[
+                  CupertinoActionSheetAction(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Do Something'),
+                  ),
+                  CupertinoActionSheetAction(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Do something else'),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
