729e253e3020289d54b041383217db81b611ade7
Jones Tavares <jotalico@icloud.com>
2023-04-21 19:59:15 -0400

    FlutterMapp | Every Widget | CupertinoTabBar

--- FlutterMapp/widgets/lib/imports.dart
+++ FlutterMapp/widgets/lib/imports.dart
@@ -70,7 +70,8 @@ import 'package:flutter/material.dart';
 // import 'package:widgets/widgets/C/cupertino_slider.dart';
 // import 'package:widgets/widgets/C/cupertino_sliding_segmentedcontrol.dart';
 // import 'package:widgets/widgets/C/cupertino_switch.dart';
-import 'package:widgets/widgets/C/cupertino_tab_scaffold.dart';
+// import 'package:widgets/widgets/C/cupertino_tab_scaffold.dart';
+import 'package:widgets/widgets/C/cupertino_tabbar.dart';
 
 // Widgets starting in D
 class Imports extends StatelessWidget {
---
+++ FlutterMapp/widgets/lib/widgets/C/cupertino_tabbar.dart
@@ -0,0 +1,42 @@
+//!CupertinoTabBar
+
+import 'package:flutter/cupertino.dart';
+
+class Scene extends StatefulWidget {
+  const Scene({super.key});
+
+  @override
+  State<Scene> createState() => _SceneState();
+}
+
+class _SceneState extends State<Scene> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return Center(
+              child: Icon(
+                index == 0 ? CupertinoIcons.home : CupertinoIcons.settings,
+                size: 80,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
