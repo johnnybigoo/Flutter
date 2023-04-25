8d5b18345095e8e11f98b49ccf4d4337c2df7631
Jones Tavares <jotalico@icloud.com>
2023-04-24 22:08:52 -0400

    FlutterMapp | Every Widget | DataCell

---
+++ FlutterMapp/widgets/lib/widgets/D/data_cell.dart
@@ -0,0 +1,69 @@
+//!DataCell
+
+import 'package:flutter/material.dart';
+
+class Scene extends StatelessWidget {
+  const Scene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle titles = const TextStyle(
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.bold,
+    );
+    return DataTable(
+      columns: [
+        DataColumn(
+          label: Text(
+            'Name',
+            style: titles,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Age',
+            style: titles,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Color',
+            style: titles,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Status',
+            style: titles,
+          ),
+        ),
+      ],
+      rows: const [
+        DataRow(
+          cells: [
+            DataCell(Text('Max')),
+            DataCell(Text('21')),
+            DataCell(Text('Red')),
+            DataCell(Text('Work')),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Jane')),
+            DataCell(Text('25')),
+            DataCell(Text('Blue')),
+            DataCell(Text('Study')),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('William')),
+            DataCell(Text('27')),
+            DataCell(Text('Green')),
+            DataCell(Text('Sleeping')),
+          ],
+        ),
+      ],
+    );
+  }
+}
