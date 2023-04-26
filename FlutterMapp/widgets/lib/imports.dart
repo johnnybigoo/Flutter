6e6e8f08fee6e14388c946a8ee92f0117f9201e9
Jones Tavares <jotalico@icloud.com>
2023-04-25 20:24:54 -0400

    FlutterMapp | Every Widget | DateRangePicker

--- FlutterMapp/widgets/lib/imports.dart
+++ FlutterMapp/widgets/lib/imports.dart
@@ -80,7 +80,8 @@ import 'package:flutter/material.dart';
 // Widgets starting in D
 // import 'package:widgets/widgets/D/data_table.dart';
 // import 'package:widgets/widgets/D/data_column.dart';
-import 'package:widgets/widgets/D/data_picker.dart';
+// import 'package:widgets/widgets/D/data_picker.dart';
+import 'package:widgets/widgets/D/date_range_picker.dart';
 
 // Widgets starting in E
 class Imports extends StatelessWidget {
