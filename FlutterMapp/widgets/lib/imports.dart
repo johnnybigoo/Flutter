3f4971328483c657937005be7935ff7d2d55ddf1
Jones Tavares <jotalico@icloud.com>
2023-04-29 18:30:18 -0400

    FlutterMapp | Every Widget | DraggableWidget

--- FlutterMapp/widgets/lib/imports.dart
+++ FlutterMapp/widgets/lib/imports.dart
@@ -88,7 +88,8 @@ import 'package:flutter/material.dart';
 // import 'package:widgets/widgets/D/dismissible.dart';
 // import 'package:widgets/widgets/D/divider.dart';
 // import 'package:widgets/widgets/D/draggable_scrollable.dart';
-import 'package:widgets/widgets/D/drag_target.dart';
+// import 'package:widgets/widgets/D/drag_target.dart';
+import 'package:widgets/widgets/D/draggable_widget.dart';
 
 // Widgets starting in E
 class Imports extends StatelessWidget {
