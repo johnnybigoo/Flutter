1f9130df95847818b7116de2b03efcc0394722f1
Jones Tavares <jotalico@icloud.com>
2023-08-03 17:04:13 -0400

    FlutterMapp | Every Widget | LinearProgressIndicator

---
+++ FlutterMapp/widgets/lib/widgets/L/linear_progress_indicator.dart
@@ -0,0 +1,50 @@
+//! LinearProgressIndicator
+
+import 'package:flutter/material.dart';
+
+class Scene extends StatefulWidget {
+  const Scene({super.key});
+
+  @override
+  State<Scene> createState() => _SceneState();
+}
+
+class _SceneState extends State<Scene> with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            LinearProgressIndicator(
+              value: controller.value,
+            ),
+            const LinearProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
