29cce2ecf9649e41ec298e40a80b38a8941ae3c6
Jones Tavares <jotalico@icloud.com>
2023-08-09 21:31:00 -0400

    FlutterMapp | Every Widget | OrientationBuilder

--- FlutterMapp/widgets/lib/imports.dart
+++ FlutterMapp/widgets/lib/imports.dart
@@ -162,7 +162,8 @@ import 'package:flutter/material.dart';
 
 // Widgets starting in O
 // import 'package:widgets/widgets/O/offstage.dart';
-import 'package:widgets/widgets/O/opacity.dart';
+// import 'package:widgets/widgets/O/opacity.dart';
+import 'package:widgets/widgets/O/orientation_builder.dart';
 
 class Imports extends StatelessWidget {
   const Imports({Key? key}) : super(key: key);
