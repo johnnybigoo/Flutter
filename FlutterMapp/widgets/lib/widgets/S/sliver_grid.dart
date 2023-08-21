37695401e6c2eb487cb9f7faa8b5a7825169dd5b
Jones Tavares <jotalico@icloud.com>
2023-08-21 11:19:31 -0400

    FlutterMapp | Every Widget | SliverGrid

---
+++ FlutterMapp/widgets/lib/widgets/S/sliver_grid.dart
@@ -0,0 +1,30 @@
+//! SliverGrid
+
+import 'package:flutter/material.dart';
+
+class Scene extends StatelessWidget {
+  const Scene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.orange[100 * (index % 9 + 1)],
+                child: Text('Item ${index + 1}'),
+              );
+            },
+            childCount: 30,
+          ),
+        )
+      ],
+    );
+  }
+}
