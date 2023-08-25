9c30ca996b6c71b250f7847618acf0feedfb82b9
Jones Tavares <jotalico@icloud.com>
2023-08-25 10:52:51 -0400

    FlutterMapp | Every Widget | VerticalDivider

--- FlutterMapp/widgets/lib/imports.dart
+++ FlutterMapp/widgets/lib/imports.dart
@@ -242,7 +242,8 @@ import 'package:flutter/material.dart';
 // import 'package:widgets/widgets/T/tween_animation_builder.dart';
 
 // Widgets starting in S
-import 'package:widgets/widgets/V/value_listenable_builder.dart';
+// import 'package:widgets/widgets/V/value_listenable_builder.dart';
+import 'package:widgets/widgets/V/vertical_divider.dart';
 
 class Imports extends StatelessWidget {
   const Imports({Key? key}) : super(key: key);
