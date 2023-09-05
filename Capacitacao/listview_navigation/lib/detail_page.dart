05f928ce28441bf30fcee0f10d6d6fb341159f19
Jones Tavares <jotalico@icloud.com>
2023-09-04 22:14:03 -0400

    Capacitacao | List Widget - 42

--- Capacitacao/listview_navigation/lib/detail_page.dart
+++ Capacitacao/listview_navigation/lib/detail_page.dart
@@ -552,7 +552,7 @@ class DetailPage extends StatelessWidget {
           ListTile(
             trailing: const Icon(Icons.arrow_forward_ios),
             title: const Text(
-              'C',
+              'ClipReact',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
               ),
